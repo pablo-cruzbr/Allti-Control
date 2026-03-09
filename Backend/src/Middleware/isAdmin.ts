a40cb8cd3c476a0b09016f86e66d9f42eedb788e
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-03-09 12:21:55 -0300

    npx prisma migrate dev --name add_role_to_users

--- Backend/src/Middleware/isAdmin.ts
+++ Backend/src/Middleware/isAdmin.ts
@@ -1,16 +1,18 @@
-import { NextFunction, Request, Response } from "express";
-import prismaClient from "../prisma";
-
 export async function isAdmin(req: Request, res: Response, next: NextFunction) {
     const user_id = req.user_id;
 
     const user = await prismaClient.user.findUnique({
-        where: {id: user_id},
-        select: {isAdmin: true}
+        where: { id: user_id },
+        select: { 
+          isAdmin: true,
+          role: true 
+        }
     });
 
-    if(!user?.isAdmin) {
-        return res.status(403).json({error: "Acesso restrito apenas administradores."})
+    // Se ele for admin pelo campo antigo OU pelo campo novo, ele passa!
+    if (user?.isAdmin === true || user?.role === "ADMIN") {
+        return next();
     }
-    return next();
+
+    return res.status(403).json({ error: "Acesso restrito apenas administradores." });
 }
